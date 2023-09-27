e02e5441d068f719c8fbc6d432c57ebec323e047
akankshaasutkar29 <akankshaasutkar2001@gmail.com>
2023-09-27 20:54:35 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "@/components/login";
+import Chat from "@/components/chat";
+function App() {
+  const [user, setUser] = useState(null);
+  const [secret, setSecret] = useState(null);
+  const isAuth = Boolean(user) && Boolean(secret);
+
+  return (
+    <>
+    <dir className="app">
+    <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuth ? (
+                <Navigate to="/chat" />
+              ) : (
+                <Login setUser={setUser} setSecret={setSecret} />
+              )
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              isAuth ? (
+                <Chat user={user} secret={secret} />
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </dir>
+     </>
+  )
+}
+
+export default App
